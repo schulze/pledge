9c933186abc3eb882b2321a975728a3bab6c0046
Frithjof Schulze <schulze@praemandatum.de>
2016-12-09 09:51:45 +0100

    Add a build constraint.

--- pledge.go
+++ pledge.go
@@ -1,10 +1,7 @@
+// +build !openbsd
+
 package pledge
 
-//#include <unistd.h>
-import "C"
-
-func Pledge(promises string, paths []string) error {
-	// TODO: Should also pass paths, but path whitelisting is not implemented yet, so no hurry.
-	_, err := C.pledge(C.CString(promises), nil)
-	return err
+func init() {
+	panic("pledge() is currently only supported on OpenBSD.")
 }
---
+++ pledge_openbsd.go
@@ -0,0 +1,10 @@
+package pledge
+
+//#include <unistd.h>
+import "C"
+
+func Pledge(promises string, paths []string) error {
+	// TODO: Should also pass paths, but path whitelisting is not implemented yet, so no hurry.
+	_, err := C.pledge(C.CString(promises), nil)
+	return err
+}
